51ea193a60ab23d06ce821a029b2793bda5cf7be
BryanDOLHIN <bryandolhin@ik.me>
2023-03-17 15:06:57 +0100

    Add category field on page entity

--- src/Form/PageType.php
+++ src/Form/PageType.php
@@ -3,19 +3,30 @@
 namespace App\Form;
 
 use App\Entity\Page;
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Symfony\Component\Form\AbstractType;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('title')
-            ->add('text',CKEditorType::class)
-        ;
+        $builder->add('title');
+        $builder->add('category', EntityType::class, [
+            'class' => Category::class,
+            
+            'query_builder' => function (CategoryRepository $er) {
+                return $er->createQueryBuilder('c')
+                    ->orderBy('c.name', 'ASC');
+            },
+            'choice_label' => 'name',
+            'multiple'=> true,
+        ]);
+        $builder->add('text',CKEditorType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
